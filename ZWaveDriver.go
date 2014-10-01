6704d6abf6a396a55109002c166d39818fa2416c
Jon Seymour <jon@ninjablocks.com>
2014-10-01 17:00:53 +1000

    Shutdown the driver in response to ZWave NODE_REMOVED events.
    Rationale: RPC layer doesn't allow us to re-add removed nodes.
    So, rather than fixing or working around this limitation, we simply exit the driver process on the expectation
    that it will be restarted.
    
    This also simplifies the recovery of resources associated with the removed node.

--- ZWaveDriver.go
+++ ZWaveDriver.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ninjasphere/go-ninja/model"
 
 	"github.com/ninjasphere/go-openzwave"
+	"github.com/ninjasphere/go-openzwave/NT"
 )
 
 const (
@@ -90,14 +91,46 @@ func (d *zwaveDriver) Start(config *ZWaveDriverConfig) error {
 		return GetLibrary().GetDeviceFactory(*node.GetProductId())(d, node)
 	}
 
+	shuttingDown := false
+
+	notificationCallback := func(api openzwave.API, nt openzwave.Notification) {
+		switch nt.GetNotificationType().Code {
+		case NT.NODE_REMOVED:
+			//
+			// Currently the RPC layer prevents us releasing the resources associated
+			// with removed nodes. If the nodes come back (when, say, the zwave controller
+			// is re-inserted), we can't build new device  wrappers for them because the
+			// devices are already registered with the RPC layer.
+			//
+			// We could fix the RPC layer or we could attempt to work around the
+			// problems with the RPC layer by using "patch" proxies for each ninja device
+			// that allows us to change the actual zwave device.
+			//
+			// For now, it is simpler if we simply restart the driver process in the event of node
+			// removal. This also avoids potential race conditions between
+			// event dispatch and freeing of the resources associated with the
+			// removed node.
+			//
+			if !shuttingDown {
+				shuttingDown = true
+				api.Logger().Infof("ZWave driver shutdown in response to node removed event.")
+				api.Shutdown(openzwave.EXIT_NODE_REMOVED)
+			}
+		default:
+
+		}
+	}
+
 	configurator := openzwave.
 		BuildAPI("/usr/local/etc/openzwave", ".", "").
 		SetLogger(log).
+		SetNotificationCallback(notificationCallback).
 		SetDeviceFactory(zwaveDeviceFactory)
 
 	if d.debug {
 		callback := func(api openzwave.API, notification openzwave.Notification) {
 			api.Logger().Infof("%v\n", notification)
+			notificationCallback(api, notification)
 		}
 
 		configurator.SetNotificationCallback(callback)
