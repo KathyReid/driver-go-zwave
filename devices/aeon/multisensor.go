252244c93aab912ac3d4d967163e08b7a63f4c20
Jon Seymour <jon@ninjablocks.com>
2014-10-02 13:44:11 +1000

    Implement the support for motion sensor.

--- devices/aeon/multisensor.go
+++ devices/aeon/multisensor.go
@@ -1,21 +1,44 @@
 package aeon
 
 import (
-	"github.com/ninjasphere/go-openzwave"
-
 	"github.com/ninjasphere/driver-go-zwave/spi"
+	"github.com/ninjasphere/go-ninja/channels"
+	"github.com/ninjasphere/go-openzwave"
+	"github.com/ninjasphere/go-openzwave/CC"
 )
 
 type multisensor struct {
-	driver spi.Driver
-	node   openzwave.Node
+	spi.Device
+	motionChannel *channels.MotionChannel
 }
 
 func MultiSensorFactory(driver spi.Driver, node openzwave.Node) openzwave.Device {
-	return &multisensor{driver, node}
+	device := &multisensor{}
+
+	device.Init(driver, node)
+
+	(*device.Info.Signatures)["ninja:thingType"] = "sensor"
+	return device
 }
 
 func (device *multisensor) NodeAdded() {
+	node := device.Node
+	api := device.Driver.ZWave()
+	conn := device.Driver.Connection()
+
+	err := conn.ExportDevice(device)
+	if err != nil {
+		api.Logger().Infof("failed to export node: %v as device: %s", node, err)
+		return
+	}
+
+	device.motionChannel = channels.NewMotionChannel(device)
+	err = conn.ExportChannel(device, device.motionChannel, "motion")
+	if err != nil {
+		api.Logger().Infof("failed to export motion channel for %v: %s", node, err)
+		return
+	}
+
 }
 
 func (device *multisensor) NodeChanged() {
@@ -24,5 +47,9 @@ func (device *multisensor) NodeChanged() {
 func (device *multisensor) NodeRemoved() {
 }
 
-func (device *multisensor) ValueChanged(openzwave.Value) {
+func (device *multisensor) ValueChanged(value openzwave.Value) {
+	switch value.Id().CommandClassId {
+	case CC.SENSOR_BINARY:
+		device.motionChannel.SendMotion()
+	}
 }
