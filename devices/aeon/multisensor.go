77f3d9d7453615ab6c45d324b1d5f10c5917bf98
Jon Seymour <jon@ninjablocks.com>
2014-10-02 22:11:43 +1000

    Add support for temperature, illuminance and humidity sensors.

--- devices/aeon/multisensor.go
+++ devices/aeon/multisensor.go
@@ -12,10 +12,20 @@ import (
 	"github.com/ninjasphere/driver-go-zwave/utils"
 )
 
+var (
+	motion_sensor      = openzwave.ValueID{CC.SENSOR_BINARY, 1, 0}
+	temperature_sensor = openzwave.ValueID{CC.SENSOR_MULTILEVEL, 1, 1}
+	illuminance_sensor = openzwave.ValueID{CC.SENSOR_MULTILEVEL, 1, 3}
+	humidity_sensor    = openzwave.ValueID{CC.SENSOR_MULTILEVEL, 1, 5}
+)
+
 type multisensor struct {
 	spi.Device
-	motionChannel *channels.MotionChannel
-	motionSensor  utils.Emitter
+	motionChannel      *channels.MotionChannel
+	motionSensor       utils.Emitter
+	temperatureChannel *channels.TemperatureChannel
+	illuminanceChannel *channels.IlluminanceChannel
+	humidityChannel    *channels.HumidityChannel
 }
 
 func MultiSensorFactory(driver spi.Driver, node openzwave.Node) openzwave.Device {
@@ -49,20 +59,62 @@ func (device *multisensor) NodeAdded() {
 		return
 	}
 
+	device.illuminanceChannel = channels.NewIlluminanceChannel(device)
+	err = conn.ExportChannel(device, device.illuminanceChannel, "illuminance")
+	if err != nil {
+		api.Logger().Infof("failed to export illuminance channel for %v: %s", node, err)
+		return
+	} else {
+		node.GetValueWithId(illuminance_sensor).SetPollingState(true)
+	}
+
+	device.temperatureChannel = channels.NewTemperatureChannel(device)
+	err = conn.ExportChannel(device, device.temperatureChannel, "temperature")
+	if err != nil {
+		api.Logger().Infof("failed to export temperature channel for %v: %s", node, err)
+		return
+	} else {
+		node.GetValueWithId(temperature_sensor).SetPollingState(true)
+	}
+
+	device.humidityChannel = channels.NewHumidityChannel(device)
+	err = conn.ExportChannel(device, device.humidityChannel, "humidity")
+	if err != nil {
+		api.Logger().Infof("failed to export humidity channel for %v: %s", node, err)
+		return
+	} else {
+		node.GetValueWithId(humidity_sensor).SetPollingState(true)
+	}
 }
 
 func (device *multisensor) NodeChanged() {
 }
 
 func (device *multisensor) NodeRemoved() {
+
 }
 
 func (device *multisensor) ValueChanged(value openzwave.Value) {
-	switch value.Id().CommandClassId {
-	case CC.SENSOR_BINARY:
+	switch value.Id() {
+	case motion_sensor: // motion
 		flag, ok := value.GetBool()
-		if ok && flag {
+		if ok && flag && device.motionChannel != nil {
 			device.motionSensor.Emit(utils.WrapBool(true))
+		}
+	case temperature_sensor: // temperature
+		valF, ok := value.GetFloat()
+		if ok && device.temperatureChannel != nil {
+			device.temperatureChannel.SendState(valF)
+		}
+	case illuminance_sensor: // luminance
+		valF, ok := value.GetFloat()
+		if ok && device.illuminanceChannel != nil {
+			device.illuminanceChannel.SendState(valF)
+		}
+	case humidity_sensor: // relative humidity
+		valF, ok := value.GetFloat()
+		if ok {
+			device.humidityChannel.SendState(valF / 100)
 		}
 	}
 }
