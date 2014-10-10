359133ce213b36c982140444325110c855161cc9
Jon Seymour <jon@ninjablocks.com>
2014-10-10 14:54:34 +1100

    Fix zwave build following release tagging.

--- scripts/build.sh
+++ scripts/build.sh
@@ -37,13 +37,12 @@ fi
 # move the working path and build
 cd .gopath/src/github.com/${OWNER}/${PROJECT_NAME} &&
 go get -d -v ./... &&
-make deps &&
-go build -ldflags "-X main.GitCommit ${GIT_COMMIT}${GIT_DIRTY} -extldflags -L${GOPATH}/src/github.com/${OWNER}/go-openzwave/openzwave" -o ${BIN_NAME} &&
-mv ${BIN_NAME} ./bin
-
-# building the master branch on ci
+make deps && (
 if [ "$BUILDBOX_BRANCH" = "master" ]; then
 	go build -ldflags "-X main.GitCommit ${GIT_COMMIT}${GIT_DIRTY}" -tags release -o ./bin/${BIN_NAME}
 else
 	go build -ldflags "-X main.GitCommit ${GIT_COMMIT}${GIT_DIRTY}" -o ./bin/${BIN_NAME}
 fi
+) 
+
+# building the master branch on ci
